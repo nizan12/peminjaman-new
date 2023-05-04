935c8c5779d88332f6c739c78b32f02594bd132d
unknown <gilangbagus.rama@gmail.com>
2023-05-04 13:12:10 +0700

    Add Room UI

--- resources/views/pages/admin/room/create.blade.php
+++ resources/views/pages/admin/room/create.blade.php
@@ -2,26 +2,25 @@
 
 
 @section('title')
-    Tambah Ruangan
+Tambah Ruangan
 @endsection
 
 {{-- Content --}}
 @section('content')
 
-<div
-    class="section-content section-dashboard-home">
+<div class="section-content section-dashboard-home">
     <div class="container-fluid">
         <div class="dashboard-content">
             <div class="row">
                 <div class="col-md-12">
                     @if($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                     @endif
                     <div class="card">
                         <div class="card-body">
@@ -31,10 +30,9 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>Kode Ruangan <small class="text-danger">*</small></label>
-                                            <input type="text" name="code" 
-                                            value="{{ old('code') }}" 
-                                            placeholder="Contoh : TA.XI.4a"
-                                            class="form-control @error('code') is-invalid @enderror">
+                                            <input type="text" name="code" value="{{ old('code') }}"
+                                                placeholder="Contoh : TA.XI.4a"
+                                                class="form-control @error('code') is-invalid @enderror">
 
                                             @error('code')
                                             <small class="invalid-feedback">{{ $message }}</small>
@@ -45,10 +43,9 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>Nama Ruangan <small class="text-danger">*</small></label>
-                                            <input type="text" name="name" 
-                                            value="{{ old('name') }}"
-                                            placeholder="Contoh : Workspace Data Security and Privacy"
-                                            class="form-control @error('name') is-invalid @enderror">
+                                            <input type="text" name="name" value="{{ old('name') }}"
+                                                placeholder="Contoh : Workspace Data Security and Privacy"
+                                                class="form-control @error('name') is-invalid @enderror">
 
 
                                             @error('name')
@@ -59,14 +56,15 @@
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Gedung <small class="text-danger">*</small></label>
-                                            <select name="building" class="form-control default-select @error('building') is-invalid @enderror">
+                                            <select name="building"
+                                                class="form-control default-select @error('building') is-invalid @enderror">
                                                 <option value="" selected disabled>- Pilih Gedung -</option>
                                                 <option value="Gedung Utama">Gedung Utama</option>
                                                 <option value="Teaching Factory">Teaching Factory</option>
                                                 <option value="Tower A">Tower A</option>
                                             </select>
 
-                                            
+
                                             @error('building')
                                             <small class="invalid-feedback">{{ $message }}</small>
                                             @enderror
@@ -80,11 +78,9 @@
 
 
                                             <div class="input-group mb-2">
-                                                <input type="number" min="0" name="capacity" 
-                                            
-                                                value="{{ old('capacity') }}"
-                                                placeholder="Contoh : 60"
-                                                class="form-control @error('capacity') is-invalid @enderror">
+                                                <input type="number" min="0" name="capacity"
+                                                    value="{{ old('capacity') }}" placeholder="Contoh : 60"
+                                                    class="form-control @error('capacity') is-invalid @enderror">
 
 
                                                 <div class="input-group-append">
@@ -101,12 +97,18 @@
                                 </div>
                                 <div class="row">
                                     <div class="col text-right">
-                                        <button class="btn btn-danger px-5">
+                                        <button type="button" class="btn btn-danger px-5"
+                                            onclick="window.location = '/room' ">
                                             Cancel
                                         </button>
 
-                                        <button class="btn btn-success px-5">
-                                            Save now
+                                        <button type="reset" class="btn btn-warning px-5">
+                                            Reset
+                                        </button>
+
+
+                                        <button type="submit" class="btn btn-success px-5">
+                                            Save
                                         </button>
                                     </div>
                                 </div>
