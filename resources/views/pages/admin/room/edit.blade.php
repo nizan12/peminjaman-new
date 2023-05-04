579df4714234b855fb8f691fa3fe3677c6cad170
unknown <gilangbagus.rama@gmail.com>
2023-05-04 14:23:06 +0700

    Building Option from Const Room Models, UI Fix

--- resources/views/pages/admin/room/edit.blade.php
+++ resources/views/pages/admin/room/edit.blade.php
@@ -33,7 +33,7 @@ Edit Ruangan
                                         <div class="form-group">
                                             <label>Kode Ruangan <small class="text-danger">*</small></label>
 
-                                            <input type="text" name="code" value="{{ old('code', $item->code ) }}"
+                                            <input type="text" required name="code" value="{{ old('code', $item->code ) }}"
                                                 placeholder="Contoh : TA.XI.4a"
                                                 class="form-control @error('code') is-invalid @enderror">
 
@@ -47,7 +47,7 @@ Edit Ruangan
                                         <div class="form-group">
                                             <label>Nama Ruangan <small class="text-danger">*</small></label>
 
-                                            <input type="text" name="name" value="{{ old('name', $item->name ) }}"
+                                            <input type="text" required name="name" value="{{ old('name', $item->name ) }}"
                                                 placeholder="Contoh : Workspace Data Security and Privacy"
                                                 class="form-control @error('name') is-invalid @enderror">
 
@@ -61,24 +61,23 @@ Edit Ruangan
                                         <div class="form-group">
                                             <label>Gedung <small class="text-danger">*</small></label>
 
-                                            <select name="building" class="form-control default-select">
+                                            <select name="building" required class="form-control default-select">
                                                 <option value="{{ $item->building }}" selected>{{ $item->building }}
                                                 </option>
                                                 <option value="" disabled>Pilih Gedung</option>
-                                                <option value="Gedung Utama">Gedung Utama</option>
-                                                <option value="Teaching Factory">Teaching Factory</option>
-                                                <option value="Tower A">Tower A</option>
+                                                @foreach(\App\Models\Room::BUILDINGS as $key => $values)
+                                                <option value="{{ $key }}">{{ $values }}</option>
+                                                @endforeach
                                             </select>
                                         </div>
                                     </div>
 
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <label>Kapasitas</label>
-
+                                            <label>Kapasitas <small class="text-danger">*</small></label>
 
                                             <div class="input-group mb-2">
-                                                <input type="number" min="0" name="capacity"
+                                                <input type="number" required min="0" name="capacity"
                                                     value="{{ old('capacity', $item->capacity) }}" class="form-control">
 
 
