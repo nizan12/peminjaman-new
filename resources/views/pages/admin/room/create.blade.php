13eb2a834629a3f51638ca1b081402355425ac31
unknown <gilangbagus.rama@gmail.com>
2023-05-04 12:46:13 +0700

    Room Add Form UI

--- resources/views/pages/admin/room/create.blade.php
+++ resources/views/pages/admin/room/create.blade.php
@@ -30,37 +30,81 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>Kode</label>
-                                            <input type="text" name="code" class="form-control">
+                                            <label>Kode Ruangan <small class="text-danger">*</small></label>
+                                            <input type="text" name="code" 
+                                            value="{{ old('code') }}" 
+                                            placeholder="Contoh : TA.XI.4a"
+                                            class="form-control @error('code') is-invalid @enderror">
+
+                                            @error('code')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
+
                                         </div>
                                     </div>
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>Nama</label>
-                                            <input type="text" name="name" class="form-control">
+                                            <label>Nama Ruangan <small class="text-danger">*</small></label>
+                                            <input type="text" name="name" 
+                                            value="{{ old('name') }}"
+                                            placeholder="Contoh : Workspace Data Security and Privacy"
+                                            class="form-control @error('name') is-invalid @enderror">
+
+
+                                            @error('name')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
                                         </div>
                                     </div>
-                                    <div class="col-md-12">
+                                    <div class="col-md-6">
                                         <div class="form-group">
-                                            <label>Gedung</label>
-                                            <select name="building" class="form-control default-select">
-                                                <option value="" selected disabled>Pilih Gedung</option>
+                                            <label>Gedung <small class="text-danger">*</small></label>
+                                            <select name="building" class="form-control default-select @error('building') is-invalid @enderror">
+                                                <option value="" selected disabled>- Pilih Gedung -</option>
                                                 <option value="Gedung Utama">Gedung Utama</option>
                                                 <option value="Teaching Factory">Teaching Factory</option>
                                                 <option value="Tower A">Tower A</option>
                                             </select>
+
+                                            
+                                            @error('building')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
                                         </div>
                                     </div>
 
-                                    <div class="col-md-12">
+                                    <div class="col-md-6">
                                         <div class="form-group">
-                                            <label>Kapasitas</label>
-                                            <input type="number" min="0" name="capacity" class="form-control">
+                                            <label>Kapasitas Ruangan</label>
+
+
+
+                                            <div class="input-group mb-2">
+                                                <input type="number" min="0" name="capacity" 
+                                            
+                                                value="{{ old('capacity') }}"
+                                                placeholder="Contoh : 60"
+                                                class="form-control @error('capacity') is-invalid @enderror">
+
+
+                                                <div class="input-group-append">
+                                                    <span class="input-group-text" id="basic-addon2">Orang</span>
+                                                </div>
+                                            </div>
+
+
+                                            @error('capacity')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col text-right">
+                                        <button class="btn btn-danger px-5">
+                                            Cancel
+                                        </button>
+
                                         <button class="btn btn-success px-5">
                                             Save now
                                         </button>
