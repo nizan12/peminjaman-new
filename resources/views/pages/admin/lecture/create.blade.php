c28f7d3200f168b59f19c860497c500d073e95b9
unknown <gilangbagus.rama@gmail.com>
2023-05-04 15:10:00 +0700

    Tambah Kolom NIK kedalam Form Tambah Dosen

--- resources/views/pages/admin/lecture/create.blade.php
+++ resources/views/pages/admin/lecture/create.blade.php
@@ -2,50 +2,89 @@
 
 
 @section('title')
-    Tambah Dosen
+Tambah Dosen
 @endsection
 
 {{-- Content --}}
 @section('content')
 
-<div
-    class="section-content section-dashboard-home"
-    >
+<div class="section-content section-dashboard-home">
     <div class="container-fluid">
         <div class="dashboard-content">
             <div class="row">
                 <div class="col-md-12">
                     @if($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                     @endif
                     <div class="card">
                         <div class="card-body">
                             <form action="{{ route('lecture.store') }}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 <div class="row">
-                                    <div class="col-md-12">
+                                    <div class="col-md-6">
                                         <div class="form-group">
-                                            <label>Kode</label>
-                                            <input type="text" name="code" class="form-control">
+                                            <label>Kode Dosen <b class="text-danger">*</b></label>
+                                            <input type="text" name="code" required 
+                                            placeholder="Contoh : HA"
+                                            value="{{ old('code') }}"
+                                            class="form-control @error('code') is-invalid @enderror">
+
+                                            @error('code')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
                                         </div>
                                     </div>
+
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>NIK Dosen <b class="text-danger">*</b></label>
+                                            <input type="text" name="nik" required 
+                                            placeholder="Contoh : 117175"
+                                            value="{{ old('nik') }}"
+                                            class="form-control @error('nik') is-invalid @enderror">
+
+                                            @error('nik')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>Nama</label>
-                                            <input type="text" name="name" class="form-control">
+                                            <label>Nama Dosen <b class="text-danger">*</b></label>
+                                            <input type="text" name="name" required 
+                                            placeholder="Contoh : Hamdani Arif, S.Pd., M.Sc"
+                                            value="{{ old('name') }}"
+                                            class="form-control @error ('name') is-invalid @enderror">
+                                        
+                                            @error('name')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
+                                        
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col text-right">
-                                        <button class="btn btn-primary px-5">
-                                            Save now
+
+                                        <button type="button" class="btn btn-danger px-5"
+                                            onclick="window.location = '/lecture' ">
+                                            Cancel
+                                        </button>
+
+                                        <button type="reset" class="btn btn-warning px-5">
+                                            Reset
+                                        </button>
+
+                                        <button class="btn btn-success px-5">
+                                            Save
                                         </button>
                                     </div>
                                 </div>
