1b8e81d78203e9cf0074d7cda9a6a198199eae67
unknown <gilangbagus.rama@gmail.com>
2023-05-04 14:19:53 +0700

    Add Const Building

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -9,6 +9,15 @@ class Room extends Model
 {
     use HasFactory;
 
+    const BUILDINGS = [
+        'Gedung Utama' => 'Gedung Utama',
+        'Tower A' => 'Tower A',
+        'Tower B' => 'Tower B',
+        'Teaching Factory' => 'Teaching Factory',
+        'Apartement' => 'Apartement',
+        'Technopreneur' => 'Technopreneur',
+    ];
+
     protected $fillable = [
         'code',
         'name',
