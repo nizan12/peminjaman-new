9c48fe804c9ca0fb7a909ba9b877600c678877fd
unknown <gilangbagus.rama@gmail.com>
2023-05-04 17:52:01 +0700

    UI Fix

--- resources/views/pages/admin/banner/create.blade.php
+++ resources/views/pages/admin/banner/create.blade.php
@@ -1,25 +1,29 @@
-<div
-    class="section-content section-dashboard-home"
-    data-aos="fade-up"
-    >
+@extends('layouts.dashboard')
+
+
+@section('title')
+Tambah Banner
+@endsection
+
+{{-- Content --}}
+@section('content')
+
+<div class="section-content section-dashboard-home">
+    
     <div class="container-fluid">
-        <div class="dashboard-heading">
-            <h2 class="dashboard-title">Banner</h2>
-            <p class="dashboard-subtitle">
-                Tambah Banner
-            </p>
-        </div>
+
         <div class="dashboard-content">
             <div class="row">
                 <div class="col-md-12">
                     @if($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                    
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                     @endif
                     <div class="card">
                         <div class="card-body">
@@ -28,21 +32,39 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>Foto</label>
+                                            <label>Foto Banner <b class="text-danger">*</b></label>
                                             <input type="file" name="photo" class="form-control">
                                         </div>
                                     </div>
                                     <div class="col-md-12">
-                                        <div class="form-group">
+                                        {{-- <div class="form-group">
                                             <label>Tampilkan</label>
                                             <input type="checkbox" name="is_show" class="form-control">
+                                        </div> --}}
+
+                                        <div class="form-group">
+                                            <div class="form-check">
+                                                <input name="is_show" class="form-check-input" type="checkbox">
+                                                <label class="form-check-label">
+                                                    Tampilkan Gambar
+                                                </label>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col text-right">
+                                        <button type="button" class="btn btn-danger px-5"
+                                            onclick="window.location = '/banner' ">
+                                            Cancel
+                                        </button>
+
+                                        <button type="reset" class="btn btn-warning px-5">
+                                            Reset
+                                        </button>
+
                                         <button class="btn btn-success px-5">
-                                            Save now
+                                            Save
                                         </button>
                                     </div>
                                 </div>
@@ -53,4 +75,6 @@
             </div>
         </div>
     </div>
-    </div>
+</div>
+
+@endsection
