e97337b8a1b35522d5d517d6b19be6290ebcaca4
unknown <gilangbagus.rama@gmail.com>
2023-05-06 09:18:05 +0700

    UI Fix, Ruangan Grouping

--- resources/views/pages/admin/product/edit.blade.php
+++ resources/views/pages/admin/product/edit.blade.php
@@ -31,50 +31,115 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>Nomor BMN</label>
-                                            <input type="text" name="nomor_bmn" value="{{ $item->nomor_bmn }}" class="form-control">
+                                            <label>Nomor BMN <b class="text-danger">*</b></label>
+                                            <input type="text" name="nomor_bmn" value="{{ old('nomor_bmn', $item->nomor_bmn) }}"
+                                                placeholder="Contoh : 3.05.02.01.002.2536"
+                                                class="form-control @error('nomor_bmn') is-invalid @enderror">
+
+                                            @error('nomor_bmn')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
                                         </div>
                                     </div>
+
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>Nama</label>
-                                            <input type="text" name="name" value="{{ $item->nama }}" class="form-control">
+                                            <label>Nama Barang <b class="text-danger">*</b></label>
+                                            <input type="text" name="name" value="{{ old('name', $item->name ) }}"
+                                                placeholder="Contoh : Meja Kantor Hitam"
+                                                class="form-control @error('name') is-invalid @enderror">
+
+                                            @error('name')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
+
                                         </div>
                                     </div>
+
+                                    <div class="col-md-12">
+                                        <hr>
+                                    </div>
+
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>User</label>
-                                            <select name="users_id" class="form-control default-select">
+                                            <label>User <b class="text-danger">*</b></label>
+                                            <select name="users_id"
+                                                class="form-control default-select @error('users_id') is-invalid @enderror">
                                                 <option value="{{ $item->users_id }}" selected>{{ $item->user->name }}</option>
-                                                <option value="" disabled>Pilih User</option>
+
+                                                <option value="" disabled>- Pilih User -</option>
                                                 @foreach ($users as $user)
-                                                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                                <option value="{{ $user->id }}">{{ $user->name }}</option>
                                                 @endforeach
                                             </select>
+
+                                            @error('users_id')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
                                         </div>
                                     </div>
 
-                                    <div class="col-md-12">
+
+                                    <div class="col-md-6">
                                         <div class="form-group">
-                                            <label>Kategori</label>
-                                            <select name="categories_id" class="form-control default-select">
+                                            <label>Kategori <b class="text-danger">*</b></label>
+                                            <select name="categories_id"
+                                                class="form-control default-select @error('categories_id') is-invalid @enderror">
                                                 <option value="{{ $item->categories_id }}" selected>{{ $item->category->name }}</option>
-                                                <option value="" disabled>Pilih Kategori</option>
+                                                
+                                                <option value="" disabled>- Pilih Kategori -</option>
                                                 @foreach ($categories as $category)
-                                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                                <option value="{{ $category->id }}">{{ $category->name }}</option>
                                                 @endforeach
                                             </select>
+
+                                            @error('categories_id')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
                                         </div>
                                     </div>
+                                    
 
-                                    <div class="col-md-12">
+
+                                    {{-- <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Ruangan <b class="text-danger">*</b></label>
+                                            <select name="rooms_id"
+                                                class="form-control default-select @error('rooms_id') is-invalid @enderror">
+                                                
+                                                <option value="" selected disabled>- Pilih Ruangan -</option>
+                                                @foreach ($rooms as $building => $buildingRooms)
+                                                <optgroup label="{{ $building }}">
+                                                    @foreach ($buildingRooms as $room)
+                                                    <option value="{{ $room->id }}">{{ $room->code }} - {{ $room->name
+                                                        }}</option>
+                                                    @endforeach
+                                                </optgroup>
+                                                @endforeach
+                                            </select>
+
+                                            @error('rooms_id')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                    </div> --}}
+
+
+
+                                    <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Ruangan</label>
                                             <select name="rooms_id" class="form-control default-select">
-                                                <option value="{{ $item->rooms_id }}" selected>{{ $item->room->name }}</option>
+                                                <option value="{{ $item->rooms_id }}" selected>{{ $item->room->code }} - {{ $item->room->name }}</option>
                                                 <option value="" disabled>Pilih Ruangan</option>
-                                                @foreach ($rooms as $room)
-                                                    <option value="{{ $room->id }}">{{ $room->code }} - {{ $room->name }}</option>
+
+                                                @foreach ($rooms as $building => $buildingRooms)
+                                                <optgroup label="{{ $building }}">
+                                                    @foreach ($buildingRooms as $room)
+                                                    <option value="{{ $room->id }}">{{ $room->code }} - {{ $room->name
+                                                        }}</option>
+                                                    @endforeach
+                                                </optgroup>
                                                 @endforeach
                                             </select>
                                         </div>
@@ -115,8 +180,18 @@
                                 </div>
                                 <div class="row">
                                     <div class="col text-right">
-                                        <button class="btn btn-primary px-5">
-                                            Save now
+                                        <button type="button" class="btn btn-danger px-5"
+                                            onclick="window.location = '/product' ">
+                                            Cancel
+                                        </button>
+
+                                        <button type="reset" class="btn btn-warning px-5">
+                                            Reset
+                                        </button>
+
+
+                                        <button type="submit" class="btn btn-success px-5">
+                                            Sunting
                                         </button>
                                     </div>
                                 </div>
