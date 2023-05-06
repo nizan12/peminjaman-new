39188f8a3b21027e6f800957f6bf6cc75ac31d3b
unknown <gilangbagus.rama@gmail.com>
2023-05-06 10:39:26 +0700

    Tambah Kolom Prodi

--- resources/views/pages/admin/course/create.blade.php
+++ resources/views/pages/admin/course/create.blade.php
@@ -2,27 +2,25 @@
 
 
 @section('title')
-    Tambah Matakuliah
+Tambah Matakuliah
 @endsection
 
 {{-- Content --}}
 @section('content')
 
-<div
-    class="section-content section-dashboard-home"
-    >
+<div class="section-content section-dashboard-home">
     <div class="container-fluid">
         <div class="dashboard-content">
             <div class="row">
                 <div class="col-md-12">
                     @if($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                     @endif
                     <div class="card">
                         <div class="card-body">
@@ -31,21 +29,67 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>Kode</label>
-                                            <input type="text" name="code" class="form-control">
+                                            <label>Kode Matakuliah <b class="text-danger">*</b></label>
+                                            <input type="text" name="code" value="{{ old('code') }}"
+                                                placeholder="Contoh : RKS214"
+                                                class="form-control @error('code') is-invalid @enderror">
+
+                                            @error('code')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
                                         </div>
                                     </div>
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>Nama</label>
-                                            <input type="text" name="name" class="form-control">
+                                            <label>Nama Matakuliah <b class="text-danger">*</b></label>
+                                            <input type="text" name="name" value="{{ old('name') }}"
+                                                placeholder="Contoh : Sistem Operasi"
+                                                class="form-control @error('name') is-invalid @enderror">
+
+                                            @error('name')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
                                         </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Program Studi <b class="text-danger">*</b></label>
+                                            <select name="prodi" class="form-control default-select @error('prodi') is-invalid @enderror">
+                                                <option value="">- Pilih Program Studi-</option>
+                                                @foreach ($JURUSAN_PRODI as $jurusan => $prodi)
+                                                <optgroup label="{{ $jurusan }}">
+                                                    @foreach ($prodi as $p)
+                                                    <option value="{{ $p }}">{{ $p }}</option>
+                                                    @endforeach
+                                                </optgroup>
+                                                @endforeach
+                                            </select>
+
+
+                                            @error('prodi')
+                                            <small class="invalid-feedback">{{ $message }}</small>
+                                            @enderror
+
+
+                                        </div>
+
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col text-right">
-                                        <button class="btn btn-primary px-5">
-                                            Save now
+                                        <button type="button" class="btn btn-danger px-5"
+                                            onclick="window.location = '/course' ">
+                                            Cancel
+                                        </button>
+
+                                        <button type="reset" class="btn btn-warning px-5">
+                                            Reset
+                                        </button>
+
+
+                                        <button type="submit" class="btn btn-success px-5">
+                                            Save
                                         </button>
                                     </div>
                                 </div>
