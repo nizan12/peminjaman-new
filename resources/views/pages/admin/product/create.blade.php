70fa7c025d5bab7048f563793eeac65b6c6ae481
unknown <gilangbagus.rama@gmail.com>
2023-05-06 09:17:40 +0700

    UI Fix, Ruangan Grouping

--- resources/views/pages/admin/product/create.blade.php
+++ resources/views/pages/admin/product/create.blade.php
@@ -2,27 +2,25 @@
 
 
 @section('title')
-    Tambah Produk
+Tambah Produk
 @endsection
 
 {{-- Content --}}
 @section('content')
 
-<div
-    class="section-content section-dashboard-home"
-    >
+<div class="section-content section-dashboard-home">
     <div class="container-fluid">
         <div class="dashboard-content">
             <div class="row">
                 <div class="col-md-12">
                     @if($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                     @endif
                     <div class="card">
                         <div class="card-body">
@@ -32,9 +30,9 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>Nomor BMN <b class="text-danger">*</b></label>
-                                            <input type="text" name="nomor_bmn" 
-                                            placeholder="Contoh : 3.05.02.01.002.2536"
-                                            class="form-control @error('nomor_bmn') is-invalid @enderror">
+                                            <input type="text" name="nomor_bmn" value="{{ old('nomor_bmn') }}"
+                                                placeholder="Contoh : 3.05.02.01.002.2536"
+                                                class="form-control @error('nomor_bmn') is-invalid @enderror">
 
                                             @error('nomor_bmn')
                                             <small class="invalid-feedback">{{ $message }}</small>
@@ -44,9 +42,9 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>Nama Barang <b class="text-danger">*</b></label>
-                                            <input type="text" name="name" 
-                                            placeholder="Contoh : Meja Kantor Hitam"
-                                            class="form-control @error('name') is-invalid @enderror">
+                                            <input type="text" name="name" value="{{ old('name') }}"
+                                                placeholder="Contoh : Meja Kantor Hitam"
+                                                class="form-control @error('name') is-invalid @enderror">
 
                                             @error('name')
                                             <small class="invalid-feedback">{{ $message }}</small>
@@ -55,15 +53,18 @@
                                         </div>
                                     </div>
 
-                                    <div class="col-md-12"><hr></div>
-                            
+                                    <div class="col-md-12">
+                                        <hr>
+                                    </div>
+
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>User <b class="text-danger">*</b></label>
-                                            <select name="users_id" class="form-control default-select @error('users_id') is-invalid @enderror">
-                                                <option value="" selected disabled>Pilih User</option>
+                                            <select name="users_id"
+                                                class="form-control default-select @error('users_id') is-invalid @enderror">
+                                                <option value="" selected disabled>- Pilih User -</option>
                                                 @foreach ($users as $user)
-                                                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                                <option value="{{ $user->id }}">{{ $user->name }}</option>
                                                 @endforeach
                                             </select>
 
@@ -76,10 +77,11 @@
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Kategori <b class="text-danger">*</b></label>
-                                            <select name="categories_id" class="form-control default-select @error('categories_id') is-invalid @enderror">
+                                            <select name="categories_id"
+                                                class="form-control default-select @error('categories_id') is-invalid @enderror">
                                                 <option value="" selected disabled>- Pilih Kategori -</option>
                                                 @foreach ($categories as $category)
-                                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                                <option value="{{ $category->id }}">{{ $category->name }}</option>
                                                 @endforeach
                                             </select>
 
@@ -92,10 +94,16 @@
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Ruangan <b class="text-danger">*</b></label>
-                                            <select name="rooms_id" class="form-control default-select @error('rooms_id') is-invalid @enderror">
+                                            <select name="rooms_id"
+                                                class="form-control default-select @error('rooms_id') is-invalid @enderror">
                                                 <option value="" selected disabled>- Pilih Ruangan -</option>
-                                                @foreach ($rooms as $room)
-                                                    <option value="{{ $room->id }}">{{ $room->code }} - {{ $room->name }}</option>
+                                                @foreach ($rooms as $building => $buildingRooms)
+                                                <optgroup label="{{ $building }}">
+                                                    @foreach ($buildingRooms as $room)
+                                                    <option value="{{ $room->id }}">{{ $room->code }} - {{ $room->name
+                                                        }}</option>
+                                                    @endforeach
+                                                </optgroup>
                                                 @endforeach
                                             </select>
 
@@ -108,8 +116,9 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>Deskripsi <b class="text-danger">*</b></label>
-                                            <input type="text" name="description" placeholder="Contoh : Meja Kantor Hitam Pendek, Ukuran 150cm x 80cm"
-                                            class="form-control @error('description') is-invalid @enderror">
+                                            <input type="text" name="description" value="{{ old('description') }}"
+                                                placeholder="Contoh : Meja Kantor Hitam Pendek, Ukuran 150cm x 80cm"
+                                                class="form-control @error('description') is-invalid @enderror">
 
                                             @error('description')
                                             <small class="invalid-feedback">{{ $message }}</small>
@@ -120,9 +129,9 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>Stock <b class="text-danger">*</b></label>
-                                            <input type="text" name="stock" 
-                                            placeholder="Contoh : 10 Buah"
-                                            class="form-control @error('stock') is-invalid @enderror">
+                                            <input type="text" name="stock" value="{{ old('stock') }}"
+                                                placeholder="Contoh : 10 Buah"
+                                                class="form-control @error('stock') is-invalid @enderror">
 
                                             @error('stock')
                                             <small class="invalid-feedback">{{ $message }}</small>
@@ -133,11 +142,11 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>Lokasi <b class="text-danger">*</b></label>
-                                            <input type="text" name="location"
-                                            placeholder="Contoh : 10 Buah"
-                                            class="form-control @error('location') is-invalid @enderror">
+                                            <input type="text" name="location" value="{{ old('location') }}"
+                                                placeholder="Contoh : Meja Instruktur"
+                                                class="form-control @error('location') is-invalid @enderror">
 
-                                            
+
                                             @error('location')
                                             <small class="invalid-feedback">{{ $message }}</small>
                                             @enderror
@@ -147,7 +156,8 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>Kondisi <b class="text-danger">*</b></label>
-                                            <select name="condition" class="form-control default-select @error('condition') is-invalid @enderror">
+                                            <select name="condition"
+                                                class="form-control default-select @error('condition') is-invalid @enderror">
                                                 <option value="" selected disabled>- Pilih Kondisi -</option>
                                                 <option value="Baik">Baik</option>
                                                 <option value="Rusak">Rusak</option>
@@ -162,7 +172,8 @@
                                 <div class="row">
                                     <div class="col text-right">
 
-                                        <button type="button" class="btn btn-danger px-5" onclick="window.location.href='/product'">
+                                        <button type="button" class="btn btn-danger px-5"
+                                            onclick="window.location.href='/product'">
                                             Cancel
                                         </button>
                                         <button type="reset" class="btn btn-warning px-5">
