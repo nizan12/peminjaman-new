ce88dc3fa5e6e91fed0526eb9028a32ea749e0a6
unknown <gilangbagus.rama@gmail.com>
2023-05-10 18:08:03 +0700

    Home Layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -50,7 +50,7 @@
 
         <div id="main-wrapper">
             <div class="nav-header">
-                <a href="{{ route('home') }}" class="brand-logo">
+                <a href="{{ route('home') }}" aria-label="Application Logo and Link to Dashboard" class="brand-logo">
                     <img class="logo-abbr" src="/images/logo.png" alt="">
                     <img class="logo-compact" src="/images/logo-text.png" alt="">
                     <img class="brand-title" src="/images/logo-text.png" alt="">
