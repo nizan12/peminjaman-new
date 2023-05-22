1ea07c1800783a0a3b0d7a4cd627c6843891f9c9
unknown <gilangbagus.rama@gmail.com>
2023-05-22 17:23:31 +0700

    Tambah Link Kelola Kelas dan Jadwal Matakuliah

--- resources/views/elements/sidebar.blade.php
+++ resources/views/elements/sidebar.blade.php
@@ -32,10 +32,11 @@
 
           <li><a href="{{ route('class.index') }}">Kelas</a></li>
           <li><a href="{{ route('course.index') }}">Matakuliah</a></li>
+          <li><a href="{{ route('course-schedule.index') }}" matchUrl="true">Jadwal Matakuliah</a></li>
           <li><a href="{{ route('lecture.index') }}">Dosen</a></li>
           <li><a href="{{ route('room.index') }}">Ruangan</a></li>
-          <li><a href="{{ route('product.index') }}">Produk</a></li>
-          <li><a href="{{ route('product-gallery.index') }}">Galeri Produk</a></li>
+          <li><a href="{{ route('product.index') }}" >Produk</a></li>
+          <li><a href="{{ route('product-gallery.index') }}" matchUrl="true">Galeri Produk</a></li>
           <li><a href="{{ route('user.index') }}">User</a></li>
         </ul>
       </li>
