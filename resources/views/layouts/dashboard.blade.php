62420a60780b191cfa6cd447fe827ef4e93edb08
unknown <gilangbagus.rama@gmail.com>
2023-05-23 13:55:02 +0700

    Tambah Notifikasi Delete SWAL

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -14,12 +14,12 @@
     <!-- Fonts -->
 
     <link href="/css/style.css" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;family=Roboto:wght@100;300;400;500;700;900&amp;display=swap" rel="stylesheet" type="text/css"/>
-    <link href="{{ asset('vendor/bootstrap-select/dist/css/bootstrap-select.min.css') }}" rel="stylesheet" type="text/css"/>
-    <link href="{{ asset('vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css"/>	
-    
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;family=Roboto:wght@100;300;400;500;700;900&amp;display=swap" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor/bootstrap-select/dist/css/bootstrap-select.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css" />
+
     {{-- <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}    
+    @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 </head>
 
 <head>
@@ -28,53 +28,88 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
-	
-	<meta name="description" content="Some description for the page"/>
+
+    <meta name="description" content="Some description for the page" />
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="http://test-peminjaman.test/images/favicon.png">
-    <link href="{{ asset('/vendor/bootstrap-select/dist/css/bootstrap-select.min.css') }}" rel="stylesheet" type="text/css"/>
-    <link href="{{ asset('/vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css"/>	
-    <link href="{{ asset('/css/style.css') }}" rel="stylesheet" type="text/css"/>
-    
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;family=Roboto:wght@100;300;400;500;700;900&amp;display=swap" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('/vendor/bootstrap-select/dist/css/bootstrap-select.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/css/style.css') }}" rel="stylesheet" type="text/css" />
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;family=Roboto:wght@100;300;400;500;700;900&amp;display=swap" rel="stylesheet" type="text/css" />
+
+    {{-- CSS untuk Sweetalert 2 --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.css">
+
 </head>
 
-    <body>
-        <div id="preloader">
-            <div class="sk-three-bounce">
-                <div class="sk-child sk-bounce1"></div>
-                <div class="sk-child sk-bounce2"></div>
-                <div class="sk-child sk-bounce3"></div>
-            </div>
+<body>
+    <div id="preloader">
+        <div class="sk-three-bounce">
+            <div class="sk-child sk-bounce1"></div>
+            <div class="sk-child sk-bounce2"></div>
+            <div class="sk-child sk-bounce3"></div>
         </div>
+    </div>
 
-        <div id="main-wrapper">
-            <div class="nav-header">
-                <a href="{{ route('home') }}" aria-label="Application Logo and Link to Dashboard" class="brand-logo">
-                    <img class="logo-abbr" src="/images/logo.png" alt="">
-                    <img class="logo-compact" src="/images/logo-text.png" alt="">
-                    <img class="brand-title" src="/images/logo-text.png" alt="">
-                	
-                </a>
+    <div id="main-wrapper">
+        <div class="nav-header">
+            <a href="{{ route('home') }}" aria-label="Application Logo and Link to Dashboard" class="brand-logo">
+                <img class="logo-abbr" src="/images/logo.png" alt="">
+                <img class="logo-compact" src="/images/logo-text.png" alt="">
+                <img class="brand-title" src="/images/logo-text.png" alt="">
 
-                <div class="nav-control">
-                    <div class="hamburger">
-                        <span class="line"></span><span class="line"></span><span class="line"></span>
-                    </div>
+            </a>
+
+            <div class="nav-control">
+                <div class="hamburger">
+                    <span class="line"></span><span class="line"></span><span class="line"></span>
                 </div>
             </div>
-
-            @include('elements.header')
-            @include('elements.sidebar')
-            <div class="content-body">
-                <!-- row -->
-                @yield('content')
-            </div>
-            
-            @include('elements.footer')
         </div>
-            
-        @include('elements.footer-scripts')
-        @stack('addon-scripts')
-    </body>
+
+        @include('elements.header')
+        @include('elements.sidebar')
+        <div class="content-body">
+            <!-- row -->
+            @yield('content')
+        </div>
+
+        @include('elements.footer')
+    </div>
+
+    @include('elements.footer-scripts')
+    @stack('addon-scripts')
+
+    {{-- JS Sweetalert 2 --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.js"></script>
+
+    <script>
+        function delete_confirm(event) {
+            event.preventDefault();
+
+            return new Promise((resolve) => {
+                swal.fire({
+                    title: 'Konfirmasi Hapus'
+                    , text: 'Anda yakin ingin menghapus data?'
+                    , icon: 'question'
+                    , showCancelButton: true
+                    , confirmButtonColor: "#DD6B55"
+                    , confirmButtonText: "Ya, hapus data!"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        resolve(true);
+                    } else {
+                        resolve(false);
+                    }
+                });
+            }).then((shouldSubmit) => {
+                if (shouldSubmit) {
+                    event.target.submit();
+                }
+            });
+        }
+
+    </script>
+</body>
 </html>
